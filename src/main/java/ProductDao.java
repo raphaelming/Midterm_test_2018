b9ba8a6c1820034b4e72ab92c157877f9d6eeae6
Lming1 <spear131121@gmail.com>
2018-04-18 23:02:10 +0900

    1번 문제

--- src/main/java/ProductDao.java
+++ src/main/java/ProductDao.java
@@ -3,8 +3,7 @@ import java.sql.*;
 
 public class ProductDao {
     public Product get(Long id) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/jeju", "jeju", "jejupw");
+        Connection connection = getConnection();
 
         PreparedStatement preparedStatement = connection.prepareStatement("select * from product where id = ?");
         preparedStatement.setLong(1, id);
@@ -23,5 +22,30 @@ public class ProductDao {
         connection.close();
 
         return product;
+    }
+
+    public Long insert(Product product) throws ClassNotFoundException, SQLException {
+        Connection connection = getConnection();
+
+        PreparedStatement preparedStatement = connection.prepareStatement("insert into product(title, price) values(?, ?)", Statement.RETURN_GENERATED_KEYS);
+        preparedStatement.setString(1, product.getTitle());
+        preparedStatement.setInt(2, product.getPrice());
+        preparedStatement.executeUpdate();
+        ResultSet resultSet = preparedStatement.getGeneratedKeys();
+        resultSet.next();
+
+        Long id = resultSet.getLong(1);
+        //자원을 해지한다.
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+
+        return id;
+
+    }
+
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        return DriverManager.getConnection("jdbc:mysql://localhost/jeju", "jeju", "jejupw");
     }
 }
--- src/test/java/ProductDaoTest.java
+++ src/test/java/ProductDaoTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import java.sql.SQLException;
@@ -5,12 +6,15 @@ import java.sql.SQLException;
 import static org.junit.Assert.assertEquals;
 
 public class ProductDaoTest {
+    private ProductDao productDao;
+    @Before
     public void setup() {
+        productDao = new ProductDao();
     }
 
     @Test
     public void get() throws SQLException, ClassNotFoundException {
-        ProductDao productDao = new ProductDao();
+
         Long id = 1L;
         String title = "제주감귤";
         Integer price = 15000;
@@ -19,5 +23,18 @@ public class ProductDaoTest {
         assertEquals(id, product.getId());
         assertEquals(title, product.getTitle());
         assertEquals(price, product.getPrice());
+    }
+
+    @Test
+    public void add() throws SQLException, ClassNotFoundException {
+        Product product = new Product();
+        product.setTitle("orange");
+        product.setPrice(20000);
+        Long id = productDao.insert(product);
+
+        Product insertedProduct = productDao.get(id);
+        assertEquals(id, insertedProduct.getId());
+        assertEquals(product.getTitle(), insertedProduct.getTitle());
+        assertEquals(product.getPrice(), insertedProduct.getPrice());
     }
 }
