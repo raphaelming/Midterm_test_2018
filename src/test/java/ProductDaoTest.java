18e50e6dda800949db4a88c5d643e692a9135cce
Lming1 <spear131121@gmail.com>
2018-04-19 01:08:38 +0900

    DaoFactory Spring

--- src/test/java/ProductDaoTest.java
+++ src/test/java/ProductDaoTest.java
@@ -1,5 +1,7 @@
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 
@@ -8,10 +10,11 @@ import static org.junit.Assert.assertEquals;
 public class ProductDaoTest {
     private ProductDao productDao;
     private DaoFactory daoFactory;
+
     @Before
     public void setup() {
-        daoFactory = new DaoFactory();
-        productDao = daoFactory.getProductDao();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(DaoFactory.class);
+        productDao = applicationContext.getBean("productDao", ProductDao.class);
     }
 
     @Test
